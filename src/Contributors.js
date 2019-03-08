dd91c903d77dad8e4ceaf147b2ba60446591585d
Chandra Kishore Danduri <chandra.kishore180994@gmail.com>
2019-03-07 19:52:19 -0500

    ReAdded (#7)
    * ReAdded
    
    * testing pushing to other PR

--- src/Contributors.js
+++ src/Contributors.js
@@ -7,7 +7,7 @@ const Contributors = () => (
     </div>
     <ul>
       <li>Ky Lee</li>
-      <li>Chandra Kishore Danduri</li>
+      <li>Chandra Kishore Danduri (Re-Added Redux)</li>
       <li>Krishna</li>
     </ul>
   </div>

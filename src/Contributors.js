a315546edc08e6e6849358310f4db4f8f94d62b3
Ky Lee <klee@quid.com>
2019-03-07 19:06:05 -0500

    add Contributors component

---
+++ src/Contributors.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contributors = () => (
+  <div>
+    <div>
+      Contributors
+    </div>
+    <ul>
+      <li>Ky Lee</li>
+    </ul>
+  </div>
+);
+
+export default Contributors;

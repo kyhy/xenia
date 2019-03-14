0c6cc04c623ebb45c6064be1baab654395fba37c
Ky Lee <klee@quid.com>
2019-03-14 19:26:40 -0400

    add emotion

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from '@emotion/styled';
+
+const Perimeter = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+  & > * {
+    padding: 1rem;
+  }
+`;
+
+const StyledLink = styled(Link)`
+`;
 
 export default function Nav() {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/contributors">Contributors</Link>
-    </div>
+    <Perimeter>
+      <StyledLink to="/">Home</StyledLink>
+      <StyledLink to="/contributors">Contributors</StyledLink>
+    </Perimeter>
   );
 }

1dc2d297fb58980930d76a2ba2aecbb50c11d914
Ky Lee <klee@quid.com>
2019-03-14 19:02:39 -0400

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Contributors from './Contributors';
+import Nav from './Nav';
+import Home from './Home';
+
+/*
+routes:
+
+/             -> home page
+/contributors -> contributor page
+
+*/
 
 function App() {
   return (
-    <div>
-      <Contributors />
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Route exact path="/" component={Home} />
+        <Route path="/contributors" component={Contributors} />
+      </div>
+    </Router>
   );
 }
 

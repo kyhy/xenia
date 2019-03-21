3cb2d89c660ab6981e47c7502988ee09b1cd37ff
SaranyaMohan03 <saramohan0406@gmail.com>
2019-03-21 19:23:15 -0400

    Added my page

--- src/Sarah.js
+++ src/Sarah.js
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import './index.css';
+import styled from '@emotion/styled'
+import icon from "./assets/saranya.jpg";
+const StyledImg = styled.img`
+border-radius: 50%;
+height: 250px;
+width: 180px;
+`;
+s
+const Parent = styled.div`
+    margin: auto;
+    text-align:center;
+`;
 const Sarah = () => (
-  <div>
-    <h1>Sarah</h1>
-  </div>
+    <Parent>
+                     
+        <StyledImg src = {icon} alt=""/>
+        <h2>Sara</h2>
+        <p>I am a graduate student doing Masters in University Of Dayton.I worked in Multinational company for three years which gained me professional experience  </p>
+    </Parent>
 );
 
 export default Sarah;

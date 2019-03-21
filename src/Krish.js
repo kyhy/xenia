f523b389dde1657cb5f7ee7b5479b582371f1ee3
krishna <mksphaninder@gmail.com>
2019-03-21 16:37:13 -0400

    designed basic contributor page for Krishna

--- src/Krish.js
+++ src/Krish.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import KrishnaPic from './krishna2.jpg';
+import './Krish.css';
+const Krish = () => (
+    <div className = "title">
+        <h1> Krishna </h1>
+        <KrishnaImage />
+        <Description />
+    </div>
+);
 
-const Krish = () => <div>Krish</div>;
+function KrishnaImage() {
+    return(
+        <div className = "Krish-img">
+            <img src={KrishnaPic} alt ="Krishna" />
+        </div>
+    );
+}
+
+function Description() {
+    return(
+        <div className="description">
+            <p>Hello my name is Krishna. I am Java developer with
+                a passion for cloud computing and learning React.js lately.
+                I love to code watching movies and play video games.
+            </p>
+        </div>
+    );
+}
 
 export default Krish;

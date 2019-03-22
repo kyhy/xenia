79693983ad84f1262a8fdb6b36168d36571a1a3b
SaranyaMohan03 <saramohan0406@gmail.com>
2019-03-21 20:05:10 -0400

    Added eslintrc

--- src/Sarah.js
+++ src/Sarah.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import './index.css';
-import styled from '@emotion/styled'
-import icon from "./assets/saranya.jpg";
+import styled from '@emotion/styled';
+import icon from './assets/saranya.jpg';
 const StyledImg = styled.img`
-border-radius: 50%;
-height: 250px;
-width: 180px;
+  border-radius: 50%;
+  height: 250px;
+  width: 180px;
 `;
-s
+s;
 const Parent = styled.div`
-    margin: auto;
-    text-align:center;
+  margin: auto;
+  text-align: center;
+  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
 `;
 const Sarah = () => (
-    <Parent>
-                     
-        <StyledImg src = {icon} alt=""/>
-        <h2>Sara</h2>
-        <p>I am a graduate student doing Masters in University Of Dayton.I worked in Multinational company for three years which gained me professional experience  </p>
-    </Parent>
+  <Parent>
+    <StyledImg src={icon} alt="" />
+    <h2>Sara</h2>
+    <p>
+      I am a graduate student doing Masters in University Of Dayton.I worked in
+      Multinational company for three years which gained me professional
+      experience{' '}
+    </p>
+  </Parent>
 );
 
 export default Sarah;
